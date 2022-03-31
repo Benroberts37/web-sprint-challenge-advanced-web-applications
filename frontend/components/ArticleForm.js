df62c60fcc9ceab4bc5df9f7c467e7b37b217681
Benroberts37 <benrobsemail@gmail.com>
2022-03-31 13:34:46 -0600

    made some progress on the articleForm page

--- frontend/components/ArticleForm.js
+++ frontend/components/ArticleForm.js
@@ -5,14 +5,19 @@ const initialFormValues = { title: '', text: '', topic: '' }
 
 export default function ArticleForm(props) {
   const [values, setValues] = useState(initialFormValues)
-  // ✨ where are my props? Destructure them here
+  const { postArticle, updateArticle, setCurrentArticleId, article } = props
 
   useEffect(() => {
+    if (article) {
+      setValues(article)
+    } else {
+      setValues(initialFormValues)
+    }
     // ✨ implement
     // Every time the `currentArticle` prop changes, we should check it for truthiness:
     // if it's truthy, we should set its title, text and topic into the corresponding
     // values of the form. If it's not, we should reset the form back to initial values.
-  })
+  }, [])
 
   const onChange = evt => {
     const { id, value } = evt.target
@@ -21,6 +26,12 @@ export default function ArticleForm(props) {
 
   const onSubmit = evt => {
     evt.preventDefault()
+    if (article) {
+      updateArticle(article.article_id, values)
+    } else {
+      postArticle(values)
+    }
+    setValues(initialFormValues)
     // ✨ implement
     // We must submit a new post or update an existing one,
     // depending on the truthyness of the `currentArticle` prop.
@@ -29,6 +40,16 @@ export default function ArticleForm(props) {
   const isDisabled = () => {
     // ✨ implement
     // Make sure the inputs have some values
+    if (values.text&&values.topic&&values.title) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  const cancelEdit = () => {
+    setValues(initialFormValues)
+    setCurrentArticleId()
   }
 
   return (
@@ -58,7 +79,7 @@ export default function ArticleForm(props) {
       </select>
       <div className="button-group">
         <button disabled={isDisabled()} id="submitArticle">Submit</button>
-        <button onClick={Function.prototype}>Cancel edit</button>
+        <button onClick={cancelEdit}>Cancel edit</button>
       </div>
     </form>
   )
